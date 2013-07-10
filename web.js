c4ad024fa9cd2edc9e3db04002f840de8b95d67c
Wale Olujide <walata@startup.d75group.net>
2013-07-10 07:42:06 -0700

    Play more with different encoding

--- web.js
+++ web.js
@@ -3,17 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buffer = new Buffer(128);
+var buffer = new Buffer(35);
 
 app.get('/', function(request, response) {
   var content = false;
   try {
      
-      content = fs.readFileSync(__dirname+'/'+'index.html', 'ascii');
+      content = fs.readFileSync(__dirname+'/'+'index.html', 'utf-8');
       buffer.write(content);
   } catch (err) {
 } 
-  response.send(buffer.toString('ascii'));
+  response.send(buffer.toString('utf-8', 0, buffer.length));
 });
 
 var port = process.env.PORT || 5000;

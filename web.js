5399c2ba2de7bae73eeee05cb0dcf108e3c3029e
Wale Olujide <walata@startup.d75group.net>
2013-07-10 09:42:52 -0700

    Correct the buffer issue

--- web.js
+++ web.js
@@ -3,17 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buffer = new Buffer(1000);
 
 app.get('/', function(request, response) {
   var content = false;
   try {
      
       content = fs.readFileSync(__dirname+'/'+'index.html', 'utf-8');
-      buffer.write(content);
   } catch (err) {
 } 
-  response.send(buffer.toString('utf-8', 0, buffer.length));
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
